56503a8a7255c6b5bd5b0ef9aad513944c30e3af
米国梁 <nothingmi@muchencute.com>
2021-04-29 16:08:11 +0800

    init

---
+++ CheckPhotoLibraryPermission/CheckPhotoLibraryPermissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckPhotoLibraryPermissionApp.swift
+//  CheckPhotoLibraryPermission
+//
+//  Created by 米国梁 on 2021/4/29.
+//
+
+import SwiftUI
+
+@main
+struct CheckPhotoLibraryPermissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckPhotoLibraryPermission/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheckPhotoLibraryPermission
+//
+//  Created by 米国梁 on 2021/4/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
